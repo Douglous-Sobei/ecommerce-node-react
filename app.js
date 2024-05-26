f79831a16763b0a901f9705818eda41fd738947a
Douglous-Sobei <douglousmangoyi@gmail.com>
2024-05-26 08:09:26 +0300

    Connecting to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("Connected to database"))
+  .catch((error) => console.error("Error connecting to database", error));
 
 app.get("/", (req, res) => {
   res.send("Hello World Updated!");
