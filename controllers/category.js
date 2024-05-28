37650d9cc2e168ebdb49f3ccfb3ddeadddf3725a
Douglous-Sobei <douglousmangoyi@gmail.com>
2024-05-28 08:08:30 +0300

    Category by id and single category

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,38 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+// Get the category By ID
+exports.categoryById = async (req, res, next, id) => {
+  try {
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(400).json({
+        error: "Category not found",
+      });
+    }
+
+    req.category = category;
+    next();
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err),
+    });
+  }
+};
+
+// single category
+exports.read = async (req, res) => {
+  try {
+    const category = req.category;
+    res.json(category);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err),
+    });
+  }
+};
+
 exports.create = async (req, res) => {
   try {
     // Assuming category data is available in the request body
--- routes/category.js
+++ routes/category.js
@@ -3,12 +3,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const { create, categoryById, read } = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
 
 router.param("userId", userById);
+router.param("categoryId", categoryById);
 
 module.exports = router;
