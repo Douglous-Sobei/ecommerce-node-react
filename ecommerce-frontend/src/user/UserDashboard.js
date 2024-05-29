05fe79e6e62e7fd35e666776d9dfb8952054599a
Douglous-Sobei <douglousmangoyi@gmail.com>
2024-05-29 19:45:48 +0300

    Links on user dahsboard

--- ecommerce-frontend/src/user/UserDashboard.js
+++ ecommerce-frontend/src/user/UserDashboard.js
@@ -1,19 +1,45 @@
 import React from "react";
 import { isAuthenticated } from "../auth";
 import Layout from "../core/Layout";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const UserDashboard = () => {
   const {
-    user: { name, email, role },
+    user: { _id, name, email, role },
   } = isAuthenticated();
+  const userLink = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header">User Links</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link className="nav-link" to="/cart">
+              My Cart
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/profile/update">
+              Update Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
 
-  return (
-    <Layout
-      title="Dashboard"
-      description="User Dashboard"
-      className="container"
-    >
+  const purchaseHistory = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header">Purchase history</h4>
+        <ul className="list-group">
+          <li className="list-group-item">history</li>
+        </ul>
+      </div>
+    );
+  };
+
+  const userInfo = () => {
+    return (
       <div className="card mb-5">
         <h3 className="card-header">User Information</h3>
         <ul className="list-group">
@@ -23,13 +49,24 @@ const UserDashboard = () => {
           <li className="list-group-item">
             {role === 1 ? "Admin" : "Registered User"}
           </li>
+          <li className="list-group-item">{_id}</li>
         </ul>
       </div>
-      <div className="card mb-5">
-        <h3 className="card-header">Purchase History</h3>
-        <ul className="list-group">
-          <li className="list-group-item">History</li>
-        </ul>
+    );
+  };
+
+  return (
+    <Layout
+      title="Dashboard"
+      description={`G'day ${name}!`}
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-3">{userLink()}</div>
+        <div className="col-9">
+          {userInfo()}
+          {purchaseHistory()}
+        </div>
       </div>
     </Layout>
   );
