2404f3f1baa477f24bb271ce1c3c7995dbd2e98a
Douglous-Sobei <douglousmangoyi@gmail.com>
2024-05-30 17:46:29 +0300

    Add category component

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -7,6 +7,7 @@ import PrivateRoute from "./auth/PrivateRoute";
 import Dashboard from "./user/UserDashboard";
 import AdminRoute from "./auth/AdminRoute";
 import AdminDashboard from "./user/AdminDashboard";
+import AddCategory from "./admin/AddCategory";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/signup" exact component={Signup} />
         <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+        <AdminRoute path="/create/category" exact component={AddCategory} />
       </Switch>
     </BrowserRouter>
   );
