9a3f29af0be3627e82f49fbebf2276f1c38b7715
Douglous-Sobei <douglousmangoyi@gmail.com>
2024-05-30 23:16:55 +0300

    Get categories in the shop page

--- ecommerce-frontend/src/core/ApiCore.js
+++ ecommerce-frontend/src/core/ApiCore.js
@@ -9,3 +9,13 @@ export const getProducts = async (sortBy) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const getCategories = async () => {
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
