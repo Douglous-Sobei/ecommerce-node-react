6b12b1d1f3aae3f5733004325a88c27b804e1aeb
Douglous-Sobei <douglousmangoyi@gmail.com>
2024-06-05 16:33:45 +0300

    Handle category toggle

--- ecommerce-frontend/src/core/Checkbox.js
+++ ecommerce-frontend/src/core/Checkbox.js
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 const Checkbox = ({ categories }) => {
+  const [checked, setChecked] = useState([]);
+  const handleToggle = (c) => () => {
+    const currentCategoryId = checked.indexOf(c);
+    const newCheckedCategoryId = [...checked];
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(c);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+  };
   return categories.map((c, i) => (
     <li key={i} className="list-unstyled">
-      <input type="checkbox" className="form-check-input" />
+      <input
+        onChange={handleToggle(c._id)}
+        value={checked.indexOf(c._id === -1)}
+        type="checkbox"
+        className="form-check-input"
+      />
       <label className="form-check-label">{c.name}</label>
     </li>
   ));
