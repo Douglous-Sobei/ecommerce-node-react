e5c7d36180c476b614b67056ea3690f174894a74
Douglous-Sobei <douglousmangoyi@gmail.com>
2024-06-05 16:11:27 +0300

    Get categories in shop page

--- ecommerce-frontend/src/core/Shop.js
+++ ecommerce-frontend/src/core/Shop.js
@@ -4,16 +4,37 @@ import Card from "./Card";
 import { ApiCore, getCategories } from "../core/ApiCore";
 
 const Shop = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(null);
+
+  const init = async () => {
+    try {
+      const data = await getCategories();
+      if (data) {
+        setCategories(data);
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <Layout
       title="Shop Page"
       description="Search and find books of your choice"
+      className="container-fluid"
     >
+      {error && <div>{error.message}</div>}
       <div className="row">
-        <div className="col-4">Left sidebar</div>
+        <div className="col-4">{JSON.stringify(categories)}</div>
         <div className="col-8">Right sidebar</div>
       </div>
     </Layout>
   );
 };
 export default Shop;
+
